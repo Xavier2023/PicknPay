2e4d707c4a69cc607fd3fd0fdbe19ef6422dbf59
Orjinta Anthony <orjintaxavier@gmail.com>
2025-01-17 03:12:12 +0100

    Form done

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,17 +1,63 @@
-import React, { useState } from 'react'
-import { FaEnvelope, FaUser } from "react-icons/fa";
-import { RiLockPasswordFill } from "react-icons/ri";
-
-
+import React, { useRef, useState } from 'react'
+import { FaEnvelope, FaUser, FaEyeSlash, FaPhone } from "react-icons/fa";
+import { IoEyeSharp } from "react-icons/io5";
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { TiWarning } from 'react-icons/ti';
 import './SignUpPage.css'
+
+const schema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string(),
+    confirmPassword: z.string()
+}).refine(data => data.password === data.confirmPassword, {
+    message: "Passwords does not match.",
+    path: ["confirmPassword"]
+})
+
 
 
 const LoginPage = () => {
 
+ const [user, setUser] = useState({
+    name: '',
+    password: ''
+ })
+
+  const {register, handleSubmit, formState: {errors}} = useForm({resolver: zodResolver(schema)})
+
+  const handleChange = (e) => {
+        const {name, value} = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+  }
+  const onLogin = () => {
+    console.log(user)
+  }
+  const onSignUp = (formData) => console.log(formData)
+  
+
   const [active , setActive] = useState(false)
+  const [showPassoword, setShowPassword] = useState('password')
+  const passowrdRef = useRef(null)
+
 
   const addActive = () => {
     setActive(true)
+  }
+
+  const handleShow = () => {
+    setShowPassword(showPassoword === 'password' ? 'text' : 'password')
+
+    if(showPassoword === 'password') {
+        passowrdRef.current.type = 'password'
+    } else {
+        passowrdRef.current.type = 'text'
+    }
   }
 
   const removeActive = () => {
@@ -25,21 +71,42 @@ const LoginPage = () => {
 
         <div className="form-box login">
             <h2 className="title animation">Login</h2>
-            <form action="#">
+            <form onSubmit={handleSubmit(onLogin)}>
 
                 <div className="input-box animation">
-                    <input type="text" required />
-                    <label htmlFor="">Username</label>
+                    <input 
+                        type="text"
+                        name='name'
+                        id='name'
+                        value={user.name}
+                        onChange={handleChange}
+                        required
+                    />
+                    <label htmlFor="name">Username</label>
                     <div className="icon">
                         <FaUser />
                     </div>
                 </div>
 
                 <div className="input-box animation">
-                    <input type="text" required />
+                    <input 
+                        type="password"
+                        name='password'
+                        id='password'
+                        value={user.password}
+                        onChange={handleChange}
+                        required 
+                         />
                     <label htmlFor="">Password</label>
-                    <div className="icon">
-                        <RiLockPasswordFill />
+                    <div className="icon" onClick={handleShow}>
+                        {
+                            showPassoword === 'password' ? (
+                                <IoEyeSharp />
+                            )  : (
+                                <FaEyeSlash />
+                            )
+                        
+                        }
                     </div>
                 </div>
                 <button type="submit" className="btn animation">Login</button>
@@ -58,10 +125,15 @@ const LoginPage = () => {
 
             <h2 className="title animation">Sign Up</h2>
 
-            <form action="#">
+            <form onSubmit={handleSubmit(onSignUp)}>
 
                 <div className="input-box animation">
-                    <input type="text" required />
+                    <input 
+                        type="text" 
+                        name='name'
+                        id='name'
+                        {...register('name')}
+                        required />
                     <label htmlFor="">Username</label>
                     <div className="icon">
                         <FaUser />
@@ -69,18 +141,57 @@ const LoginPage = () => {
                 </div>
 
                 <div className="input-box animation">
-                    <input type="email" name="" id="" required />
-                    <label htmlFor="">Email</label>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        {...register('email')}
+                        required />
+                    <label htmlFor="email">Email</label>
                     <div className="icon">
                         <FaEnvelope />
                     </div>
                 </div>
 
                 <div className="input-box animation">
-                    <input type="password" required />
+                    <input 
+                        type="password"
+                        name='password'
+                        id='password'
+                        {...register('password')}
+                        required  />
                     <label htmlFor="">Password</label>
-                    <div className="icon">
-                        <RiLockPasswordFill />
+                    <div className="icon" onClick={handleShow}>
+                        {
+                            showPassoword === 'password' ? (
+                                <IoEyeSharp />
+                            )  : (
+                                <FaEyeSlash />
+                            )
+                        
+                        }
+                    </div>
+                </div>
+
+                <div className="input-box animation">
+                    <input 
+                        type="password" 
+                        name="confirmPassword" 
+                        id="cpassword"
+                        {...register('confirmPassword')}
+                        required 
+                    />
+                    {errors.confirmPassword && <em className='error-msg'>{errors.confirmPassword.message}<TiWarning /></em>}
+                    <label htmlFor="cpassword">Confirm Password</label>
+                    <div className="icon" onClick={handleShow}>
+                        {
+                            showPassoword === 'password' ? (
+                                <FaEyeSlash />
+                            )  : (
+                                <IoEyeSharp />
+                            )
+                        
+                        }
                     </div>
                 </div>
 
--- src/components/Authentication/SignUpPage.jsx
+++ src/components/Authentication/SignUpPage.jsx
@@ -1,12 +1,61 @@
-import React, { useState } from 'react'
-import { FaEnvelope, FaUser } from "react-icons/fa";
-import { RiLockPasswordFill } from "react-icons/ri";
-
+import React, { useRef, useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { FaEnvelope, FaEyeSlash, FaPhone, FaUser } from "react-icons/fa";
+import { IoEyeSharp } from 'react-icons/io5';
+import { TiWarning } from "react-icons/ti";
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod';
 import './SignUpPage.css'
 
+
+export const schema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string(),
+    confirmPassword: z.string()
+}).refine(data => data.password === data.confirmPassword, {
+    message: "Passwords does not match.",
+    path: ["confirmPassword"]
+})
 const SignUpPage = () => {
 
+    const [user, setUser] = useState({
+        name: '',
+        password: ''
+     })  
+
+  const {register, handleSubmit, formState: {errors}} = useForm({resolver: zodResolver(schema)})
+  const onSignUp = (formData) => console.log(formData)
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setUser({
+        ...user,
+        [name]: value
+    })
+}
+const onLogin = (e) => {
+    e.preventDefault()
+console.log(user)
+}
+
   const [active , setActive] = useState(false)
+  const [showPassoword, setShowPassword] = useState('password')
+  const passowrdRef = useRef(null)
+
+   
+  const handleShow = () => {
+    setShowPassword(showPassoword === 'password' ? 'text' : 'password')
+
+    if(showPassoword === 'password') {
+        passowrdRef.current.type = 'password'
+    } else {
+        passowrdRef.current.type = 'text'
+    }
+
+    // console.log(showPassoword);
+    
+    
+  }
 
   const addActive = () => {
     setActive(true)
@@ -23,10 +72,15 @@ const SignUpPage = () => {
 
         <div className="form-box login">
             <h2 className="title animation">Sign Up</h2>
-            <form action="#">
+            <form onSubmit={handleSubmit(onSignUp)}>
 
                 <div className="input-box animation">
-                    <input type="text" required />
+                    <input 
+                        type="text"
+                        name='name'
+                        id='name'
+                        {...register('name')}
+                     />
                     <label htmlFor="">Username</label>
                     <div className="icon">
                         <FaUser />
@@ -34,7 +88,12 @@ const SignUpPage = () => {
                 </div>
 
                 <div className="input-box animation">
-                    <input type="email" name="" id="" required />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email"
+                        {...register('email')}
+                        required />
                     <label htmlFor="">Email</label>
                     <div className="icon">
                         <FaEnvelope />
@@ -42,10 +101,43 @@ const SignUpPage = () => {
                 </div>
 
                 <div className="input-box animation">
-                    <input type="text" required />
+                    <input 
+                        type="password"  
+                        name='password'
+                        id='password'
+                        {...register('password')}
+                        required />
                     <label htmlFor="">Password</label>
-                    <div className="icon">
-                        <RiLockPasswordFill />
+                    <div className="icon" onClick={handleShow}>
+                        {
+                            showPassoword === 'password' ? (
+                                <FaEyeSlash />
+                            )  : (
+                                <IoEyeSharp />
+                            )
+                        
+                        }
+                    </div>
+                </div>
+                <div className="input-box animation">
+                    <input 
+                        type="password" 
+                        name="confirmPassword" 
+                        id="cpassword"
+                        {...register('confirmPassword')}
+                        required 
+                    />
+                    {errors.confirmPassword && <em className='error-msg'>{errors.confirmPassword.message}<TiWarning /></em>}
+                    <label htmlFor="cpassword">Confirm Password</label>
+                    <div className="icon" onClick={handleShow}>
+                        {
+                            showPassoword === 'password' ? (
+                                <FaEyeSlash />
+                            )  : (
+                                <IoEyeSharp />
+                            )
+                        
+                        }
                     </div>
                 </div>
                 <button type="submit" className="btn animation">Sign Up</button>
@@ -64,11 +156,20 @@ const SignUpPage = () => {
 
             <h2 className="title animation">Login</h2>
 
-            <form action="#">
+            <form onSubmit={onLogin}>
 
                 <div className="input-box animation">
-                    <input type="text" required />
-                    <label htmlFor="">Username</label>
+                    <input 
+                        type="text"
+                        name='name'
+                        id='name'
+                        value={user.name}
+                        onChange={handleChange}
+                        required
+                        minLength={3}
+                        maxLength={8}
+                    />
+                    <label htmlFor="name">Username</label>
                     <div className="icon">
                         <FaUser />
                     </div>
@@ -76,10 +177,25 @@ const SignUpPage = () => {
                 
 
                 <div className="input-box animation">
-                    <input type="password" required />
-                    <label htmlFor="">Password</label>
-                    <div className="icon">
-                        <RiLockPasswordFill />
+                    <input 
+                        type="password" 
+                        name='password'
+                        id='password'
+                        value={user.password}
+                        onChange={handleChange}
+                        ref={passowrdRef}
+                        minLength={8} 
+                        required />
+                    <label htmlFor="password">Password</label>
+                    <div className="icon" onClick={handleShow}>
+                        {
+                            showPassoword === 'password' ? (
+                               <FaEyeSlash />
+                            )  : (
+                                <IoEyeSharp />
+                            )
+                        
+                        }
                     </div>
                 </div>
 
