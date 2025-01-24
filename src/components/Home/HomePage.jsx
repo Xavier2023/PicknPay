2549df01064af21707e2c3a080a45b1d578d8d3f
Orjinta Anthony <orjintaxavier@gmail.com>
2025-01-24 20:58:23 +0100

    id update

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -9,13 +9,13 @@ const HomePage = () => {
     <div>
         <HeroSection 
          title='Buy iPhone 14 pro'
-         link='/products/678a974e4ffa8dc0fba022d7'
+         link='/products/6793a474271f34d9ca2199bc'
          image={iphone16}
          subtitle='The iPhone 14 Pro is a marvel of engineering, combining cutting-edge technology with an elegant design.' />
         <FlashSales />
         <HeroSection 
          title="M4, M4 Pro, and M4 Max.Ready for whatever’s impossible."
-         link='/products/678a974e4ffa8dc0fba022df'
+         link='/products/6793a474271f34d9ca2199c4'
          image={macBook}
          subtitle=' Built for Apple Intelligence. Up to 24 hours of battery life. Liquid Retina XDR display' />
     </div>
